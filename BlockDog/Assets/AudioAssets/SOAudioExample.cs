eb2ee3b8f081512e682cdb2974b18e26faa0f971
super8ude <crbertelsen@gmail.com>
2021-10-12 16:46:04 -0400

    Updates to project version, some comments

--- BlockDog/Assets/AudioAssets/SOAudioExample.cs
+++ BlockDog/Assets/AudioAssets/SOAudioExample.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //This is just to demonstrate what an alternative pattern would look like, using scriptable objects
-//I would use something similar to this if I was going to make a game that would scale larger
+//I would use something similar to this if I was going to make a game that would scale larger, and didn't want to use something like FMOD
+//The main advantage of this is that we can put our volume and randomization settings togehter with an array of audio clips; if we had all these for every event on our Audio Manager it would get very cluttered.
+
+//Another option is to use a different prefab for every sound effect, and put these settings into a custom monobehavior. 
 
 [CreateAssetMenu(menuName = "AudioObject")]
 public class SOAudioExample : ScriptableObject
@@ -18,6 +21,7 @@ public class SOAudioExample : ScriptableObject
     private int lastClipPlayed = -1;
 
     //usually you don't want to repeat the last clip played.  you could extend this to include "shuffle bag" style randomization.
+    //see https://gamedevelopment.tutsplus.com/tutorials/shuffle-bags-making-random-feel-more-random--gamedev-1249
     public AudioClip PickClip()
     {
         if(playSequentially)
